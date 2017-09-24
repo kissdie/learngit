42f625a9d7710137d3f601e0b993bba7e03200a6
苏灿 <chencstj@163.com>
2017-09-24 20:52:54 +0800

    完成导航及底部

--- first/first/urls.py
+++ first/first/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from home.views import index as home_index
+from home.views import index as music_index
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r"^navigation/templates/(?P<path>.*)", 'django.views.static.serve', {"document_root":"/home/su/django/website/first/navigation/templates"}),
+    url(r'^$', home_index, name="daohang"),
+    url(r'^music/$', music_index, name="music")
 ]
---
+++ first/home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+
+    return render(request, "home.html",)
+
+def music(request):
+
+    return render(request, "wenzhang.html")
